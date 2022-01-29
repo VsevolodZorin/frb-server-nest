3139791a7db6ea7b75d106da87de6ae6cd434bac
Vsevolod Zorin <vsevolod.dev@gmail.com>
2022-01-29 14:21:18 +0200

    add .env configModule get

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from '@src/app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get()
   getHello(): string {
+    this.configService.get('TEST');
     return this.appService.getHello();
   }
 }
