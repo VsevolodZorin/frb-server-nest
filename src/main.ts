5d057ba06062bd29647347e0793036c38b5e8228
Vsevolod Zorin <vsevolod.dev@gmail.com>
2022-01-29 14:17:16 +0200

    add .env configModule

--- src/main.ts
+++ src/main.ts
@@ -7,30 +7,30 @@ import * as express from 'express';
 import * as functions from 'firebase-functions';
 import { AppModule } from '@src/app.module';
 
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   await app.listen(4000);
-//   app.setGlobalPrefix('api');
-//   app.enableCors({ origin: true });
-// }
-//
-// bootstrap();22
-
-const server = express();
-
-export const createNestServer = async (expressInstance) => {
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(expressInstance),
-  );
-  // app.setGlobalPrefix('api');
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(4000);
+  app.setGlobalPrefix('api');
   app.enableCors({ origin: true });
+}
 
-  return app.init();
-};
+bootstrap();
 
-createNestServer(server)
-  .then((v) => console.log('Nest Ready'))
-  .catch((err) => console.error('Nest broken', err));
-
-export const api = functions.https.onRequest(server);
+// const server = express();
+//
+// export const createNestServer = async (expressInstance) => {
+//   const app = await NestFactory.create(
+//     AppModule,
+//     new ExpressAdapter(expressInstance),
+//   );
+//   // app.setGlobalPrefix('api');
+//   app.enableCors({ origin: true });
+//
+//   return app.init();
+// };
+//
+// createNestServer(server)
+//   .then((v) => console.log('Nest Ready'))
+//   .catch((err) => console.error('Nest broken', err));
+//
+// export const api = functions.https.onRequest(server);
