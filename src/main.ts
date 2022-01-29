e64704c15ed97d3e56518730ecd68a8dd5d9109e
Vsevolod Zorin <vsevolod.dev@gmail.com>
2022-01-29 13:55:06 +0200

    add firebase functions

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,36 @@
 if (!process.env.IS_TS_NODE) {
-  require('module-alias/register')
+  require('module-alias/register');
 }
-import {NestFactory} from '@nestjs/core';
-import {AppModule} from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import * as functions from 'firebase-functions';
+import { AppModule } from '@src/app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
-}
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(4000);
+//   app.setGlobalPrefix('api');
+//   app.enableCors({ origin: true });
+// }
+//
+// bootstrap();22
 
-bootstrap();
+const server = express();
+
+export const createNestServer = async (expressInstance) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+  // app.setGlobalPrefix('api');
+  app.enableCors({ origin: true });
+
+  return app.init();
+};
+
+createNestServer(server)
+  .then((v) => console.log('Nest Ready'))
+  .catch((err) => console.error('Nest broken', err));
+
+export const api = functions.https.onRequest(server);
