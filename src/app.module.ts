cbc7559b41b452b2f6abfd895c4ee94ae4319fd5
Vsevolod Zorin <vsevolod.dev@gmail.com>
2022-01-31 00:43:25 +0200

    refactor firebase-app as service

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,11 +9,13 @@ import { AppService } from '@src/app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from '@src/auth/auth.module';
 import { AuthFrbMiddleware } from '@src/auth/middlewares/authFrb.middleware';
+import { UserModule } from './user/user.module';
+import { FirebaseApp } from '@src/firebase/firebase-app';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule],
+  imports: [ConfigModule.forRoot(), AuthModule, UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [FirebaseApp, AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
--- src/auth/middlewares/authFrb.middleware.ts
+++ src/auth/middlewares/authFrb.middleware.ts
@@ -1,39 +1,20 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response } from 'express';
 import * as firebase from 'firebase-admin';
-import { ConfigService } from '@nestjs/config';
+import { FirebaseApp } from '@src/firebase/firebase-app';
 
 @Injectable()
 export class AuthFrbMiddleware implements NestMiddleware {
-  private defaultApp: any;
+  private frbAuth: firebase.auth.Auth;
 
-  constructor(private readonly configService: ConfigService) {
-    const firebase_params = {
-      type: this.configService.get('type'),
-      projectId: this.configService.get('project_id'),
-      privateKeyId: this.configService.get('private_key_id'),
-      privateKey: this.configService.get('private_key').replace(/\\n/g, '\n'),
-      clientEmail: this.configService.get('client_email'),
-      clientId: this.configService.get('client_id'),
-      authUri: this.configService.get('auth_uri'),
-      tokenUri: this.configService.get('token_uri'),
-      authProviderX509CertUrl: this.configService.get(
-        'auth_provider_x509_cert_url',
-      ),
-      clientC509CertUrl: this.configService.get('client_x509_cert_url'),
-    };
-
-    this.defaultApp = firebase.initializeApp({
-      credential: firebase.credential.cert(firebase_params),
-      databaseURL: 'https://fir-auth-bd895.firebaseio.com',
-    });
+  constructor(private firebaseApp: FirebaseApp) {
+    this.frbAuth = firebaseApp.getAuth();
   }
 
   use(req: Request, res: Response, next: () => void) {
     const token = req.headers.authorization;
     if (token != null && token != '') {
-      this.defaultApp
-        .auth()
+      this.frbAuth
         .verifyIdToken(token.replace('Bearer ', ''))
         .then(async (decodedToken) => {
           const user = {
